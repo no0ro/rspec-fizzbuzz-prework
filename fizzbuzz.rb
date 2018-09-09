f914e64489c938fd690df9d78731275c396ceeab
Noelle Rolwes <nrolwes@gmail.com>
2018-09-09 00:20:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
   if num%3 == 0
     puts "Fizz"
-  if num%5 == 0 
+  elseif num%5 == 0 
     puts "Buzz"
-  if (num%3 && num%5) == 0 
+  elseif (num%3 && num%5) == 0 
     puts "FizzBuzz"
   else 
     puts "Nil"
