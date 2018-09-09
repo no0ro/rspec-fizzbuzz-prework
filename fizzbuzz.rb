fcadf9442e7ec8183c5eb4bb6c9c8b4f38aef04e
Noelle Rolwes <nrolwes@gmail.com>
2018-09-09 00:26:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@ def fizzbuzz(num)
   fizz3 = num%3 
   buzz5 = num%5 
   both = fizz3 && buzz5
-  if fizz3 == 0
+  if fizz3 = 0
     puts "Fizz"
-  elseif buzz5 == 0
+  elseif buzz5 = 0
     puts "Buzz"
-  elseif both == 0
+  elseif both = 0
     puts "FizzBuzz"
   else 
     puts "Nil"
