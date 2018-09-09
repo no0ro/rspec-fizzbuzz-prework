2733343318897735767324b175fbb7e6ddc0da61
Noelle Rolwes <nrolwes@gmail.com>
2018-09-09 00:32:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,9 @@
 def fizzbuzz(num)
-  fizz_3 = num%3 
-  buzz_5 = num%5 
-  fizz_15 = fizz_3 && buzz_5
-  if fizz_3 == 0
+  if num%3==0
     puts "Fizz"
-  elsif buzz_5 == 0
+  elsif num%5==0
     puts "Buzz"
-  elsif fizz_15 == 0
+  elsif num%3 && num%5 == 0
     puts "FizzBuzz"
   else 
     puts num
